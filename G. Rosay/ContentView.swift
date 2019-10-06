9ff7de5b31172857ed7048012169155f0446406f
GRosay <gaspard@rosay.ch>
2019-10-06 14:12:54 +0200

    Initial Commit

---
+++ G. Rosay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  G. Rosay
+//
+//  Created by Gaspard Rosay on 06.10.19.
+//  Copyright © 2019 Gaspard Rosay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
